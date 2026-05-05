9ae30202c156d1b510eda445b8d0145061588406
João Navarro <navarrojoao05@gmail.com>
2026-05-05 11:21:32 +0100

    supabase link atualizado

--- Sea Salt/js/supabase.js
+++ Sea Salt/js/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabaseUrl = 'https://yzvtqoxgiywqhkkyuqcj.supabase.co/rest/v1'
+const supabaseUrl = 'https://yzvtqoxgiywqhkkyuqcj.supabase.co';
 const supabaseKey = 'sb_publishable_LCEZmdACiqQJdwUSW5FEGw_IeuQ5jAl'
 export const supabase = createClient(supabaseUrl, supabaseKey)
